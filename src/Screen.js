40f38ad28d49d2bd25e4dbdf0867b5b8ab57b791
strikers1999 <mk.jung@xsight.in>
2021-04-16 12:47:48 +0900

    React create

---
+++ src/Screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Screen = () => {
+  return (
+    <div>
+      <Header />
+      This is Screen Component
+    </div>
+  );
+};
+
+export default Screen;
